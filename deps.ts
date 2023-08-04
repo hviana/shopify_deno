6b714ddaedced05b6b66e9f04fa96e013fda46c7
Henrique <hv5088@gmail.com>
2023-08-04 11:37:40 -0300

    update deps

--- deps.ts
+++ deps.ts
@@ -1,6 +1,6 @@
-export * as b64 from "https://deno.land/std@0.184.0/encoding/base64.ts";
-export * as hex from "https://deno.land/std@0.184.0/encoding/hex.ts";
-export { crypto } from "https://deno.land/std@0.184.0/crypto/mod.ts";
+export * as b64 from "https://deno.land/std@0.197.0/encoding/base64.ts";
+export * as hex from "https://deno.land/std@0.197.0/encoding/hex.ts";
+export { crypto } from "https://deno.land/std@0.197.0/crypto/mod.ts";
 
 export { Server } from "https://deno.land/x/faster/mod.ts";
 export type { Context, NextFunc } from "https://deno.land/x/faster/mod.ts";
